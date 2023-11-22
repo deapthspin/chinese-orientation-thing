9e72e31838ffac839be5dbf7bcb1040566df2161
deapthspin <thedeapthspin@gmail.com>
2023-11-22 16:33:11 +0800

    added new stuff

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 import data from './data.json'
+import { FormControl, FormControlLabel, Switch } from '@mui/material';
 
 function App() {
   const [alpha, setAlpha] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   const [chosenChar, setChosenChar] = useState('')
   const [chosenCharAns, setChosenCharAns] = useState('')
   const [chosenCharDef, setChosenCharDef] = useState('')
+  const [difficulty, setDifficulty] = useState(false)
   const handleClick = () => {
     if (typeof DeviceMotionEvent.requestPermission === 'function') {
       // Handle iOS 13+ devices.
@@ -29,18 +31,27 @@ function App() {
     }
   }
 
+  
+
   function chooseimage() {
     
     // console.log(data)
+    const infant = data.filter((item) => item.hsk_levl === 1)
     const rand = Math.floor(Math.random() * 30)
-    setChosenChar(data[rand].charcter)
-    setChosenCharAns(data[rand].pinyin)
-    setChosenCharDef(data[rand].definition)
+    if (difficulty) {
+      setChosenChar(data[rand].charcter)
+      setChosenCharAns(data[rand].pinyin)
+      setChosenCharDef(data[rand].definition)
+    } else {
+      setChosenChar(infant[rand].charcter)
+      setChosenCharAns(infant[rand].pinyin)
+      setChosenCharDef(infant[rand].definition)
+    }
+
   }
 
-  function showans() {
+    
 
-  }
 
   function handleOrientation(event) {
     const a = event.alpha;
@@ -54,13 +65,23 @@ function App() {
     if(gamma <= 45 && gamma >= -45 && (beta > 170 || beta < -170)) {
       chooseimage()
     }
-    if(beta < 3 && beta >= -1 && gamma <= 20 && gamma >= -20) {
-      showans()
-    }
+    // if(beta < 3 && beta >= -1 && gamma <= 20 && gamma >= -20) {
+    //   showans()
+    // }
     
   }, [beta, gamma])
+
+  function changeDifficulty(e) {
+    setDifficulty(!difficulty)
+  }
+
   return (
     <div className="App">
+      <FormControl>
+        <FormControlLabel control={<Switch onChange={changeDifficulty} />} label={difficulty ? 'adult' : 'infant'}/>
+      </FormControl>
+      <br/>
+      <br/>
       <button onClick={handleClick}>enable</button>
       <br/>
       <br/>
@@ -68,7 +89,12 @@ function App() {
       <h2>beta: {Math.round(beta)}</h2>
       <h2>gamma: {Math.round(gamma)}</h2> */}
       {/* {} */}
-      {beta < 3 && beta >= -1 && gamma <= 20 && gamma >= -20 && <div><h1 className='pinyin'>{chosenCharAns}</h1><br/><h2>{chosenCharDef}</h2></div>}
+      {beta < 3 && beta >= -1 && gamma <= 20 && gamma >= -20 && <div>
+        <h1 className='pinyin'>{chosenCharAns}</h1>
+        {/* <button onClick={showans}>speak</button> */}
+        <br/>
+        <h2>{chosenCharDef}</h2>
+      </div>}
       {/* {beta < 95 && beta > 80 && <h1>upright</h1>} */}
       {((beta <= 20 && beta >= -20) || (beta >= 160 && beta <= -160)) && ((gamma <= -60 && gamma >= -90) || (gamma <= 90 && gamma >= 60)) && <h1 className='char'>{chosenChar}</h1>}
       {gamma <= 45 && gamma >= -45 && (beta > 170 || beta < -170) && <div>
